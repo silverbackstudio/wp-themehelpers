8cf1dbe9752521e161a8622f332cd35e6446053f
SilverBack Studio <amministrazione@silverbackstudio.it>
2017-02-22 13:33:36 +0000

    added Tiled gallery

---
+++ src/Gallery/Tiled.php
@@ -0,0 +1,39 @@
+<?php 
+
+namespace Svbk\WP\Helpers\Gallery;
+
+// $('.gallery-size-tiled-gallery .gallery-item').each(function(){
+//   var $img = $(this).find('img'); 
+//   var ratio = parseInt($img.attr('width')) / (parseInt($img.attr('height'))*0.7);
+//   $(this).addClass('picture-ratio-'+Math.round(ratio));
+// });
+
+class Tiled {
+    
+    
+    static function gallery($html){
+        
+        if(strpos($html, 'gallery-size-tiled-gallery') === false){
+            return $html;
+        }
+    
+        //$html = str_replace( 'gallery ', 'gallery', $html );
+    
+        return $html;
+    }    
+    
+    public static function register(){
+        
+        add_image_size( 'tiled-gallery', 9999, 1040 );
+        add_filter('image_size_names_choose', array(__CLASS__, 'custom_image_sizes') );
+        
+    }
+    
+    public static function custom_image_sizes($sizes) {
+    	
+    	$sizes['tiled-gallery'] = __( "Tiled Gallery", 'svbk-themehelper');
+    	
+    	return $sizes;
+    }
+    
+}
