33c312d641977fd774b4db67c85d044762c5c4f9
SilverBack Studio <amministrazione@silverbackstudio.it>
2017-02-24 15:00:51 +0000

    fixed analytics loading

--- src/Theme.php
+++ src/Theme.php
@@ -20,6 +20,10 @@ class Theme {
         add_action('acf/init', array($this, 'acf_init'));    
         add_shortcode('bloginfo', array($this, 'bloginfo_shortcode') );
         add_shortcode('privacy-link', array($this, 'get_privacy_link') );
+        
+        add_action('wp_head', array($this, 'add_analytics'), 1);
+        add_action('after_body_tag', array($this, 'print_analytics_noscript'));
+        add_action('wp_footer', array($this, 'print_analytics_noscript'));        
     }
     
     static function init($config_file='config.php'){
@@ -251,12 +255,13 @@ class Theme {
             $dataLayer = $this->conf('google-tag-manager', 'dataLayer') ?: 'dataLayer';
             
             ?>
+            <!-- Google Tag Manager -->
             <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','<?php echo $dataLayer; ?>','<?php echo $this->conf('google-tag-manager', 'id'); ?>');</script>   
-            
+            })(window,document,'script','<?php echo $dataLayer; ?>','<?php echo $this->conf('google-tag-manager', 'id'); ?>');</script>
+            <!-- End Google Tag Manager -->
             <?php
             
         }
@@ -271,7 +276,8 @@ class Theme {
     
         ?>
         <!-- Google Tag Manager -->
-        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=<?php echo $this->conf('google-tag-manager', 'id'); ?>" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>        
+        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=<?php echo $this->conf('google-tag-manager', 'id'); ?>" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager -->
         <?php
     }
     
