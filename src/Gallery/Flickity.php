ba4fdddb08602a23e83e0f86fb204e5a67f4000e
SilverBack Studio <amministrazione@silverbackstudio.it>
2017-02-24 14:03:23 +0000

    fixed static propery in Flickity

--- src/Gallery/Flickity.php
+++ src/Gallery/Flickity.php
@@ -6,23 +6,56 @@ use Svbk\WP\Helpers\CdnScripts;
 
 class Flickity {
     
-    static function gallery($html){
+    public $width = 1320;
+    public $height = 400;
+    public $crop = false;
+    public $name = 'post-slider';
+    public $label;
+    
+    public static $_options = array(
+        'cellSelector'=>'.gallery-item',
+        'imagesLoaded'=>'true',
+        'contain'=> true,
+        'pageDots' => false,
+        'prevNextButtons'=>true
+	);
+	
+	public $options = array();
+    
+    public function __construct($properties=array()){
+        foreach($properties as $property => $value){
+            if(property_exists($this, $property)) {
+                $this->$property = $value;
+            }
+        }         
         
-        if(strpos($html, 'gallery-size-post-slider') === false){
+        if(!$this->label){
+            $this->label = __('Post Slider','svbk-themehelper');
+        }
+        
+        if(isset($properties['options'])){
+            $this->options = wp_parse_args($properties['options'], self::$_options);
+        } else {
+            $this->options = self::$_options;
+        }
+        
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_script'), 12, 2 );
+        
+        add_filter( 'gallery_style',  array($this, 'gallery') );    
+        add_filter( 'image_size_names_choose', array($this, 'custom_image_sizes') );
+        
+        add_image_size( $this->name, $this->width, $this->height, $this->crop );        
+        
+    }
+    
+    function gallery($html){
+        
+        if(strpos($html, 'gallery-size-'.$this->name) === false){
             return $html;
         }
         
-    	$extra_data = array(
-    		'data-flickity-options' => array(
-                'cellSelector'=>'.gallery-item',
-                'imagesLoaded'=>'true',
-                'contain'=> true,
-                'pageDots' => false,
-                'prevNextButtons'=>true
-    			)
-    		);
-    
-    
+    	$extra_data = array('data-flickity-options' => $this->options);
+    	
     	foreach ( (array) $extra_data as $data_key => $data_values ) {
     		$html = str_replace( '<div ', '<div ' . esc_attr( $data_key ) . "='" . json_encode( $data_values ) . "' ", $html );
     	}
@@ -32,7 +65,7 @@ class Flickity {
         return $html;
     }
     
-    public static function custom_image_sizes( $sizes ){
+    public function custom_image_sizes( $sizes ){
     	$custom_sizes = array(
     		'post-slider'	=>	__('Post Slider','svbk-themehelper')
     	);
@@ -44,14 +77,11 @@ class Flickity {
         wp_enqueue_style('flickity');
     }
 
-    static function register(){
+    static function register($properties=array()){
         
-        add_action('wp_enqueue_scripts', array(__CLASS__, 'enqueue_script'), 12, 2 );
+        $class = get_called_class();
         
-        add_filter( 'gallery_style',  array(__CLASS__, 'gallery') );    
-        add_filter( 'image_size_names_choose', array(__CLASS__, 'custom_image_sizes') );
-        
-        add_image_size( 'post-slider', 1320, 400, false );
+        return new $class($properties);
     }
     
 }
