a11737a153f2672f848fe9a4e2297b977eec5eb1
SilverBack Studio <amministrazione@silverbackstudio.it>
2017-05-08 13:13:48 +0000

    added Log\Email

--- src/Payment/PayPal.php
+++ src/Payment/PayPal.php
@@ -8,10 +8,14 @@ use PayPal\Api\VerifyWebhookSignature;
 use PayPal\Api\WebhookEvent;
 use WP_Error;
 use WP_REST_Request;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Psr\Log\LogLevel;
 
 class PayPal {
 
     protected $apiContext;
+    protected $logger;
 
     public static $sandbox = true;
 
@@ -40,6 +44,13 @@ class PayPal {
         if( $this->apiContext && !empty( $params['config'] ) ) {
             $this->apiContext->setConfig( $params['config'] );            
         } 
+        
+        $this->logger = new NullLogger;
+        
+    }
+
+    public function setLogger( LoggerInterface $logger ){
+        $this->logger = $logger;
     }
 
     public static function getEndpointUrl( $map, $sandbox = true){
@@ -54,9 +65,8 @@ class PayPal {
     
     public function verifyWebhook( WP_REST_Request $request, $webhook_id ) {
  
-        $fp = fopen('request.log', 'a');
-        fwrite($fp, "\n" . date('r') . " - WebhookEvent Request received" );
-
+        $log = " - WebhookEvent Request received";
+        
         try {
             
             $signatureVerification = new VerifyWebhookSignature();
@@ -68,14 +78,15 @@ class PayPal {
             $signatureVerification->setTransmissionTime( $request->get_header('Paypal-Transmission-Time') );
             
         } catch( InvalidArgumentException $ex ){
-            fwrite($fp, " -- INVALID HEADERS!");
+            $log .= " -- INVALID HEADERS!";
+            $this->logger->debug( $log );
             return new WP_Error( 'invalid_request', 'Not a valid WebHook request', array( 'status' => 500 ) );
         }
         
         $webhookEvent = new WebhookEvent();
         $webhookEvent->fromArray( $request->get_json_params() );
         
-        fwrite($fp, ' -- ' . $webhookEvent->getSummary() );
+        $log .= ' -- ' . $webhookEvent->getSummary();
         
         $signatureVerification->setWebhookEvent($webhookEvent);
         $PPrequest = clone $signatureVerification;
@@ -83,25 +94,26 @@ class PayPal {
         try {
             $output = $signatureVerification->post( $this->apiContext );
         } catch (Exception $ex) {
-            fwrite($fp, " -- ERROR: " . $ex->getMessage() );
+            $log .= " -- ERROR: " . $ex->getMessage();
+            $this->logger->debug( $log );
             return new WP_Error( 'invalid_payment_info', 'Could not contact payment provider:' . $ex->getMessage(), array( 'status' => 500 ) );
         }
         
         if( 'SUCCESS' !== $output->getVerificationStatus() ){
-            fwrite($fp, " -- NOT VALIDATED: " . $output->getVerificationStatus() );
+            $log .= " -- NOT VALIDATED: " . $output->getVerificationStatus() ;
+            $this->logger->debug( $log );
             return new WP_Error( 'invalid_payment_info', 'Invalid Payment Info'  , array( 'status' => 200 ) );
         }
         
-        fwrite($fp, " -- VALIDATED!");
-        fclose($fp);  
-
+        $log .= " -- VALIDATED!" ;
+        $this->logger->debug( $log );
+    
         return true;
     }
 
     public function verifyIPN( WP_REST_Request $request ){
 
-        $fp = fopen('request.log', 'a');
-        fwrite($fp, "\n" . date('r') . " - Paypal IPN Request received" );    
+        $log = " - Paypal IPN Request received";    
         
         $response = wp_remote_post(
             self::getEndpointUrl( self::IPN_ENDPOINT ),
@@ -116,13 +128,17 @@ class PayPal {
         );
     
         if ( is_wp_error( $response ) ){
+            $log .= 'ERROR:' . $response->get_error_message();
+            $this->logger->debug( $log );
             return $response;
         }
     
         $result = wp_remote_retrieve_body( $response );
     
-        fwrite($fp, ' -- STATUS: ' . $result );
-        fwrite($fp, print_r( $request->get_params(), true  ) );
+        $log .= ' -- STATUS: ' . $result;
+        $log .= print_r( $request->get_params(), true );
+        
+        $this->logger->debug( $log );
         
         if( strcmp ($result, self::IPN_VALID ) == 0 ) {
             return true;
@@ -130,7 +146,6 @@ class PayPal {
             return new WP_Error( 'invalid_payment_info', 'Invalid Payment Info'  , array( 'status' => 200 ) );
         }
             
-        fclose($fp);  
     }    
     
     public static function buttonUrl( $button_id, $args = array() ){
