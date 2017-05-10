880bfe03daa93152cab2770f9a052407f579d95e
SilverBack Studio <amministrazione@silverbackstudio.it>
2017-05-10 18:38:31 +0000

    added checkboxes and image fields, added support for arrays and minor fixes

--- src/Form/Form.php
+++ src/Form/Form.php
@@ -75,13 +75,20 @@ class Form {
     
     public function renderField($fieldName, $fieldAttr, $errors = array()){
         
+            if( ! is_array( $fieldAttr ) ){
+                return $fieldAttr;
+            }
+        
             $type = isset( $fieldAttr['type'] ) ? $fieldAttr['type'] : 'text';
             $fieldLabel = isset( $fieldAttr['label'] ) ? $fieldAttr['label'] : 'text';
             $value = isset( $fieldAttr['default'] ) ? $fieldAttr['default'] : '';
         
+            $fieldClass = preg_replace('/\[\d+\]/i', '', $fieldName);
+            $fieldClass = preg_replace('/\[(\w+)\]/i', '-$1', $fieldClass);
+
             $classes = array_merge(
                 array( 
-                    $this->field_prefix . self::PREFIX_SEPARATOR . $fieldName . '-group',
+                    $this->field_prefix . self::PREFIX_SEPARATOR . $fieldClass . '-group',
                     'field-group',
                 ),
                 isset( $fieldAttr['class'] ) ? (array)$fieldAttr['class'] : array()
@@ -93,6 +100,8 @@ class Form {
         
             $fieldNameHash = esc_attr( $this->fieldName($fieldName) );
             $fieldId =  esc_attr( $this->fieldName($fieldName, false) );
+            $fieldId = preg_replace('/\[(\d+)\]/i', '-$1', $fieldId);
+            $fieldId = preg_replace('/\[(\w+)\]/i', '-$1', $fieldId);        
         
             $output = '<div class="' . esc_attr( join(' ', $classes) ) . '">';
             
@@ -102,13 +111,28 @@ class Form {
             }
             elseif('textarea' === $type){
                     $output .=  '<label for="' . $fieldId . '">' . $fieldLabel .'</label>'
-                    .           '<textarea type="' . esc_attr($type)  . '" name="' . $fieldNameHash . '" id="' . $fieldId . '"  />'. esc_html($value) . '</textarea>';
+                    .           '<textarea type="' . esc_attr($type)  . '" name="' . $fieldNameHash . '" id="' . $fieldId . '">'. esc_html($value) . '</textarea>';
             } elseif ( ('select' === $type) && !empty($fieldAttr['choices']) ){
                     $output .=  '<label for="' . $fieldId . '">' . $fieldLabel .'</label>';
                     $output .=  '<select name="' . $fieldNameHash . '" id="' . $fieldId . '" >';
-                    foreach( $fieldAttr['choices']  as $cValue => $cLabel)
-                    $output .=  '  <option value="' . esc_attr($cValue) . '" ' . selected( $value, $cValue, false ) . '  />'. esc_html($cLabel) . '</option>';
+                    foreach( $fieldAttr['choices']  as $cValue => $cLabel) {
+                        $output .=  '  <option value="' . esc_attr($cValue) . '" ' . selected( $value, $cValue, false ) . '>'. esc_html($cLabel) . '</option>';
+                    }
                     $output .=  '</select>';
+            } elseif ( ('checkboxes' === $type) && !empty($fieldAttr['choices']) ){
+                    $output .=  '<label >' . $fieldLabel .'</label>';
+                    $output .=  '<div name="' . $fieldNameHash . '" id="' . $fieldId . '" >';
+                    foreach( $fieldAttr['choices']  as $cValue => $cLabel) {
+                        $output .=  '  <div class="select field-pair">';
+                        $output .=  '  <input id="' . $fieldId . '_' . esc_attr($cValue) . '"  name="' . $fieldNameHash . '[' . $cValue . ']" type="checkbox" value="1" ' . checked( $value, $cValue, false ) . '  />';
+                        $output .=  '  <label for="' . $fieldId . '_' . esc_attr($cValue) . '">' . esc_html($cLabel) . '</label>';
+                        $output .=  '  </div>';
+                    }
+                    $output .=  '</div>';
+            } elseif('image' === $type){
+                    $output .= '<label for="' . $fieldId . '">' . $fieldLabel .'</label>';
+                    $output .= '<input type="file" name="' . $fieldNameHash . '" id="' . $fieldId . '" />';
+                    $output .= wp_get_attachment_image( $value, 'thumb' );
             } else {
                     $output .= '<label for="' . $fieldId . '">' . $fieldLabel .'</label>'
                     .       '<input type="' . esc_attr($type)  . '" name="' . $fieldNameHash . '" id="' . $fieldId . '" value="' . esc_attr($value) . '" />';
