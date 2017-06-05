77fd0879a6d2b04a4088a2cd34fad328ffb64471
SilverBack Studio <amministrazione@silverbackstudio.it>
2017-06-05 14:11:56 +0000

    use AdSense Themehelper functions

--- src/Ads/AdSense.php
+++ src/Ads/AdSense.php
@@ -5,6 +5,42 @@ namespace Svbk\WP\Helpers\Ads;
 class AdSense {
 
     public static $script_added = false;
+    public $google_ad_client = '';
+    
+    public function __construct( $google_ad_client ){
+        $this->google_ad_client = $google_ad_client;
+    }
+    
+    public static function adunit_sizes(){
+        return array(
+		    'auto' => __('Adaptive', 'svbk-helpers'),
+		    '728x90' => __('Leaderboard', 'svbk-helpers'),
+		    '336x280' => __('Large Rectangle', 'svbk-helpers'),
+		    '300x600' => __('Large SkyScraper', 'svbk-helpers'),        
+        );
+    }
+    
+    public function adunit_code( $ad_slot, $ad_size = 'auto' ){
+        
+        if( ! $this->google_ad_client || ! $ad_slot ) {
+            return;
+        }           
+        
+        if( 'auto' == $ad_size  ){
+            $style = 'display:block;';
+            $format = 'data-ad-format="auto"';
+        } else {
+            $dims = explode('x', $ad_size);
+            $style = 'display:inline-block;width:' . $dims[0] . 'px;height:' . $dims[1] . 'px;';
+            $format = '';
+        }
+            
+        $output = '<ins class="adsbygoogle" style="' . $style . '" data-ad-client="' . $this->google_ad_client . '" data-ad-slot="' . $ad_slot . '" ' . $format . '></ins>';
+        $output .= '<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';        
+        
+        return $output;
+        
+    }
     
     public static function add_scripts( $options = array() ){
         
