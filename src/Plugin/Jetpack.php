da6bf5df0aea97895badea42534e56c056a20408
SilverBack Studio <amministrazione@silverbackstudio.it>
2017-06-20 10:15:54 +0000

    added functionto remove jetpack sharing CSS

--- src/Plugin/Jetpack.php
+++ src/Plugin/Jetpack.php
@@ -7,6 +7,21 @@ use WP_Query;
 
 class Jetpack {
 
+    public static function disableSharingCss(){
+        add_filter('pre_option_sharedaddy_disable_resources', '__return_true', 99);
+        
+        add_action( 'wp_enqueue_scripts', array(__CLASS__, 'addSharingJS') );        
+    }
+
+    public static function addSharingJS() {
+        wp_enqueue_script( 'sharing-js', WP_SHARING_PLUGIN_URL . 'sharing.js', array( ), 4 );
+        $sharing_js_options = array(
+            'lang'   => get_base_recaptcha_lang_code(),
+            'counts' => apply_filters( 'jetpack_sharing_counts', true )
+        );
+        wp_localize_script( 'sharing-js', 'sharing_js_options', $sharing_js_options );
+    }
+
     public static function contentShareRemove() {
         add_action( 'loop_start', function(){
             remove_filter( 'the_content', 'sharing_display', 19 );
