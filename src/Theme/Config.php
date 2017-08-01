523157c52a0b7fc04abbd6cb088d141476764f71
Brando Meniconi <amministrazione@silverbackstudio.it>
2017-08-01 15:27:05 +0000

    added confi set

--- src/Theme/Config.php
+++ src/Theme/Config.php
@@ -4,6 +4,8 @@ namespace Svbk\WP\Helpers\Theme;
 
 class Config {
 
+	public static $expire = HOUR_IN_SECONDS;
+
 	public static function load_config( $config_file = 'config.php' ) {
 
 		$newconfig = array();
@@ -12,7 +14,7 @@ class Config {
 
 		if ( file_exists( $file ) ) {
 			$newconfig = include_once( $file );
-			wp_cache_set( 'config', $newconfig, 'wpthemehelper', HOUR_IN_SECONDS );
+			wp_cache_set( 'config', $newconfig, 'wpthemehelper', self::$expire );
 		}
 
 		return $newconfig;
@@ -38,6 +40,31 @@ class Config {
 		}
 
 		return $default;
+	}
+
+	public static function set( $value, $group = null, $param = null ) {
+
+		$found = null;
+		$config = wp_cache_get( 'config', 'wpthemehelper', false, $found );
+
+		if ( ! $found ) {
+			$config = self::load_config();
+		}
+
+		if ( $group ) {
+	
+			if ( $param ) {
+				$config[ $group ][ $param ] = $value;
+			} else {
+				$config[ $group ] = $value;
+			}
+			
+		} else {
+			$config = $value;
+		}
+
+		wp_cache_replace( 'config', $config, 'wpthemehelper', self::$expire );
+
 	}
 
 }
