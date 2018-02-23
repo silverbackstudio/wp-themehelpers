818ecf0b51596ed3e7e7578495aca957b8308a31
Brando Meniconi <amministrazione@silverbackstudio.it>
2018-02-23 10:42:01 +0000

    splitted lists and core translations

--- plugin.php
+++ plugin.php
@@ -18,6 +18,7 @@ Text Domain: svbk-helpers
 
 function svbk_helpers_init() {
 	load_plugin_textdomain( 'svbk-helpers', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	load_plugin_textdomain( 'svbk-helpers-lists', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 }
 
 add_action( 'plugins_loaded', 'svbk_helpers_init' );
