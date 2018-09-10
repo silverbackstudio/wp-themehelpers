af24abd59113e2d69a9dd9cb0e16ead74a0a7b47
Brando Meniconi <amministrazione@silverbackstudio.it>
2018-09-10 09:37:52 +0000

    added Theme/Setup::remove_emoji

--- src/Theme/Setup.php
+++ src/Theme/Setup.php
@@ -272,5 +272,25 @@ class Setup {
 		return apply_filters( 'policy_link', '', $link_name, $attr );
 	}
 
+	/**
+	 * Disable the emoji's callback
+	 */
+	public static function disable_emojis_callback() {
+		
+	 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	 remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	 remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+	 remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	 remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+	 remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	}
+	
+	/**
+	 * Disable the emoji's
+	 */
+	public static function disable_emojis() {
+		add_action( 'init', array( self::class, 'disable_emojis_callback' ) );	
+	}	
 	
 }
